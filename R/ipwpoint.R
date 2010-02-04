b58976b618ca4379d639890192e9b1091a00bbd1
Willem M. van der Wal <w.m.vanderwal@amc.uva.nl>
2010-02-04 00:00:00 +0000

    version 1.0

---
+++ R/ipwpoint.R
@@ -0,0 +1,166 @@
+ipwpoint <- function(
+	exposure,
+	family,
+	link,
+	numerator = NULL,
+	denominator,
+	data,
+	trim = NULL,
+	...)
+	{
+		#save input
+			tempcall <- match.call()
+		#some basic input checks
+			if (!("exposure" %in% names(tempcall))) stop("No exposure variable specified")
+			if (!("family" %in% names(tempcall)) | ("family" %in% names(tempcall) & !(tempcall$family %in% c("binomial", "multinomial", "ordinal", "gaussian")))) stop("No valid family specified (\"binomial\", \"multinomial\", \"ordinal\", \"gaussian\")")
+			if (tempcall$family == "binomial") {if(!(tempcall$link %in% c("logit", "probit", "cauchit", "log", "cloglog"))) stop("No valid link function specified for family = binomial (\"logit\", \"probit\", \"cauchit\", \"log\", \"cloglog\")")}
+			if (tempcall$family == "ordinal" ) {if(!(tempcall$link %in% c("logit", "probit", "cauchit", "cloglog"))) stop("No valid link function specified for family = binomial (\"logit\", \"probit\", \"cauchit\", \"cloglog\")")}
+			if (!("denominator" %in% names(tempcall))) stop("No denominator model specified")
+			if (!is.null(tempcall$numerator) & !is(eval(tempcall$numerator), "formula")) stop("Invalid numerator formula specified")
+			if (!is.null(tempcall$denominator) & !is(eval(tempcall$denominator), "formula")) stop("Invalid denominator formula specified")
+			if (tempcall$family %in% c("gaussian") & !("numerator" %in% names(tempcall))) stop("Numerator necessary for family = \"gaussian\"")
+			if (!("data" %in% names(tempcall))) stop("No data specified")
+			if (!is.null(tempcall$trim)) {if(tempcall$trim < 0 | tempcall$trim > 0.5) stop("Invalid trim percentage specified (0-0.5)")}
+		#make new dataframe for newly computed variables, to prevent variable name conflicts
+			tempdat <- data.frame(
+				exposure = data[,as.character(tempcall$exposure)]
+			)
+		#weights binomial
+			require(stats)
+			if (tempcall$family == "binomial") {
+				if(tempcall$link == "logit") lf <- binomial(link = logit)
+				if(tempcall$link == "probit") lf  <- binomial(link = probit)
+				if(tempcall$link == "cauchit") lf  <- binomial(link = cauchit)
+				if(tempcall$link == "log") lf  <- binomial(link = log)
+				if(tempcall$link == "cloglog") lf  <- binomial(link = cloglog)
+				if (is.null(tempcall$numerator)) tempdat$w.numerator <- 1
+				else {
+					mod1 <- glm(
+						formula = eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$numerator), sep = ""))),
+						family = lf,
+						data = data,
+						na.action = na.fail,
+						...)
+					tempdat$w.numerator <- vector("numeric", nrow(tempdat))
+					tempdat$w.numerator[tempdat$exposure == 0] <- 1 - predict.glm(mod1, type = "response")[tempdat$exposure == 0]
+					tempdat$w.numerator[tempdat$exposure == 1] <- predict.glm(mod1, type = "response")[tempdat$exposure == 1]
+						mod1$call$formula <- eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$numerator), sep = "")))
+						mod1$call$family <- tempcall$link
+						mod1$call$data <- tempcall$data
+				}
+				mod2 <- glm(
+					formula = eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$denominator), sep = ""))),
+					family = lf,
+					data = data,
+					na.action = na.fail,
+					...)
+				tempdat$w.denominator <- vector("numeric", nrow(tempdat))
+				tempdat$w.denominator[tempdat$exposure == 0] <- 1 - predict.glm(mod2, type = "response")[tempdat$exposure == 0]
+				tempdat$w.denominator[tempdat$exposure == 1] <- predict.glm(mod2, type = "response")[tempdat$exposure == 1]
+				mod2$call$formula <- eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$denominator), sep = "")))
+				mod2$call$family <- tempcall$link
+				mod2$call$data <- tempcall$data
+				tempdat$ipw.weights <- tempdat$w.numerator/tempdat$w.denominator
+			}
+		#weights multinomial
+			require(nnet)
+			if (tempcall$family == "multinomial") {
+				if (is.null(tempcall$numerator)) tempdat$p.numerator <- 1
+				else {
+					mod1 <- multinom(
+						formula = eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$numerator), sep = ""))),
+						data = data,
+						na.action = na.fail,
+						...)
+					pred1 <- as.data.frame(predict(mod1, type = "probs"))
+					tempdat$w.numerator <- vector("numeric", nrow(tempdat))
+					for (i in 1:length(unique(tempdat$exposure)))tempdat$w.numerator[with(tempdat, exposure == sort(unique(tempdat$exposure))[i])] <- pred1[tempdat$exposure == sort(unique(tempdat$exposure))[i],i]
+					mod1$call$formula <- eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$numerator), sep = "")))
+					mod1$call$data <- tempcall$data
+								}
+				mod2 <- multinom(
+					formula = eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$denominator), sep = ""))),
+					data = data,
+					na.action = na.fail,
+					...)
+				pred2 <- as.data.frame(predict(mod2, type = "probs"))
+				tempdat$w.denominator <- vector("numeric", nrow(tempdat))
+				for (i in 1:length(unique(tempdat$exposure)))tempdat$w.denominator[with(tempdat, exposure == sort(unique(tempdat$exposure))[i])] <- pred2[tempdat$exposure == sort(unique(tempdat$exposure))[i],i]
+				mod2$call$formula <- eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$denominator), sep = "")))
+				mod2$call$data <- tempcall$data
+				tempdat$ipw.weights <- tempdat$w.numerator/tempdat$w.denominator
+			}
+		#weights ordinal
+			require(MASS)
+			if (tempcall$family == "ordinal") {
+				if(tempcall$link == "logit") m <- "logistic"
+				if(tempcall$link == "probit") m  <- "probit"
+				if(tempcall$link == "cloglog") m  <- "cloglog"
+				if(tempcall$link == "cauchit") m  <- "cauchit"
+				if (is.null(tempcall$numerator)) tempdat$p.numerator <- 1
+				else {
+					mod1 <- polr(
+						formula = eval(parse(text = paste("as.factor(", deparse(tempcall$exposure), ")", deparse(tempcall$numerator), sep = ""))),
+						data = data,
+						method = m,
+						na.action = na.fail,
+						...)
+					pred1 <- as.data.frame(predict(mod1, type = "probs"))
+					tempdat$w.numerator <- vector("numeric", nrow(tempdat))
+					for (i in 1:length(unique(tempdat$exposure)))tempdat$w.numerator[with(tempdat, exposure == sort(unique(tempdat$exposure))[i])] <- pred1[tempdat$exposure == sort(unique(tempdat$exposure))[i],i]
+					mod1$call$formula <- eval(parse(text = paste("as.factor(", deparse(tempcall$exposure), ")", deparse(tempcall$numerator), sep = "")))
+					mod1$call$data <- tempcall$data
+					mod1$call$method <- m
+				}
+				mod2 <- polr(
+					formula = eval(parse(text = paste("as.factor(", deparse(tempcall$exposure), ")", deparse(tempcall$denominator), sep = ""))),
+					data = data,
+					method = m,
+					na.action = na.fail,
+					...)
+				pred2 <- as.data.frame(predict(mod2, type = "probs"))
+				tempdat$w.denominator <- vector("numeric", nrow(tempdat))
+				for (i in 1:length(unique(tempdat$exposure)))tempdat$w.denominator[with(tempdat, exposure == sort(unique(tempdat$exposure))[i])] <- pred2[tempdat$exposure == sort(unique(tempdat$exposure))[i],i]
+				mod2$call$formula <- eval(parse(text = paste("as.factor(", deparse(tempcall$exposure), ")", deparse(tempcall$denominator), sep = "")))
+				mod2$call$data <- tempcall$data
+				mod2$call$method <- m
+				tempdat$ipw.weights <- tempdat$w.numerator/tempdat$w.denominator
+			}
+		#weights gaussian
+			if (tempcall$family == "gaussian") {
+				mod1 <- glm(
+					formula = eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$numerator), sep = ""))),
+					data = data,
+					na.action = na.fail,
+					...)
+				tempdat$w.numerator <- dnorm(tempdat$exposure, predict(mod1), sd(mod1$residuals))
+				mod1$call$formula <- eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$numerator), sep = "")))
+				mod1$call$data <- tempcall$data
+				mod2 <- glm(
+					formula = eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$denominator), sep = ""))),
+					data = data,
+					na.action = na.fail,
+					...)
+				tempdat$w.denominator <- dnorm(tempdat$exposure, predict(mod2), sd(mod2$residuals))
+				mod2$call$formula <- eval(parse(text = paste(deparse(tempcall$exposure), deparse(tempcall$denominator), sep = "")))
+				mod2$call$data <- tempcall$data
+				tempdat$ipw.weights <- tempdat$w.numerator/tempdat$w.denominator
+			}
+		#check for NA's in weights
+			if (sum(is.na(tempdat$ipw.weights)) > 0) stop ("NA's in weights!")
+		#trim weights, when trim value is specified (0-0.5)
+			if (!(is.null(tempcall$trim))){
+				tempdat$weights.trimmed <- tempdat$ipw.weights
+				tempdat$weights.trimmed[tempdat$ipw.weights <= quantile(tempdat$ipw.weights, 0+trim)] <- quantile(tempdat$ipw.weights, 0+trim)
+				tempdat$weights.trimmed[tempdat$ipw.weights >  quantile(tempdat$ipw.weights, 1-trim)] <- quantile(tempdat$ipw.weights, 1-trim)
+			}
+		#return results in the same order as the original input dataframe
+			if (is.null(tempcall$trim)){
+				if (is.null(tempcall$numerator)) return(list(ipw.weights = tempdat$ipw.weights, call = tempcall, den.mod = mod2))
+				else return(list(ipw.weights = tempdat$ipw.weights, call = tempcall, num.mod = mod1, den.mod = mod2))
+			}
+			else{
+				if (is.null(tempcall$numerator)) return(list(ipw.weights = tempdat$ipw.weights, weights.trimmed = tempdat$weights.trimmed, call = tempcall, den.mod = mod2))
+				else return(list(ipw.weights = tempdat$ipw.weights, weights.trimmed = tempdat$weights.trimmed, call = tempcall, num.mod = mod1, den.mod = mod2))
+			}
+}
---
+++ R/tstartfun.R
@@ -0,0 +1,36 @@
+tstartfun <- function(
+	id,
+	timevar,
+	data)
+	{	
+		#save input
+			tempcall <- match.call()
+		#record original order of dataframe so that the output can be returned in the same order
+			order.orig <- 1:nrow(data)
+			order.orig <- order.orig[order(
+				eval(parse(text = paste("data$", deparse(tempcall$id), sep = ""))),
+				eval(parse(text = paste("data$", deparse(tempcall$timevar), sep = "")))
+				)] #sort as below
+		#sort dataframe on follow-up time within each individual, necessary for cumulative products below
+			data <- data[order(
+				eval(parse(text = paste("data$", deparse(tempcall$id), sep = ""))),
+				eval(parse(text = paste("data$", deparse(tempcall$timevar), sep = "")))
+				),]
+		#make new dataframe for newly computed variables, to prevent variable name conflicts
+			tempdat <- data.frame(
+				id = data[,as.character(tempcall$id)],
+				timevar = data[,as.character(tempcall$timevar)]
+			)
+		#compute tstart
+			#time of previous record when there is a previous record
+			#zero for first record, when time of first record is after zero
+			#-1 for first record, when time of first record is zero
+			tempfun <- function(x){
+				tempdif <- diff(c(0, x))
+				tempdif[tempdif == 0] <- 1
+				return(x - tempdif)
+				}
+			tempdat$tstart <- unsplit(lapply(split(tempdat$timevar, tempdat$id), function(x)tempfun(x)), tempdat$id)
+		#return results in the same order as the original input dataframe
+			return(tstart = tempdat$tstart[order(order.orig)])
+}
